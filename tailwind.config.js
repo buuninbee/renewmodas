b85bab42c9cfe7c1977779ed195b29e3b9b2964e
buuninbee <buuninbee@gmail.com>
2024-10-11 11:12:31 -0300

    :tada: commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary:' #386A20',
+      onPrimary: '#FFFFFF',
+      segundary:' #556C4F',
+      onSegundary: '#19281F',
+      primaryContainer: '#B8F397',
+      segundaryContainer: '#042100',
+      onSurface: '#49454e' 
+    },
+    backgroundImage: {
+      card1: "url('/assets/card-1.png')",
+      card2: "url('/assets/card-2.png')",
+      card3: "url('/assets/card-3.png')"
+    },
+    fontFamily: {
+      'sans': ['Inria Sans', 'sans-serif'],
+      'serif': ['Rufina', 'serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
